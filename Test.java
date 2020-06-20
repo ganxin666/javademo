edd966e0a3369bb534a25e0771a37fa53074faf8
ganxin666 <1134896853@qq.com>
2020-06-20 14:59:40 +0800

    add file

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test {
+          public static void main(String[ ] args){ 
+  
+                System.out.println("hello world!");
+                System.out.println("hello world!");
+                System.out.println("hello world!");
+                System.out.println("hello world!");
+                System.out.println("ÄãºÃ,ÊÀ½ç!");
+               }
+}
