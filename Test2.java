58086b1be5580c8cd14e0ae4edd230e93265e1bb
ganxin666 <1134896853@qq.com>
2020-06-20 16:23:16 +0800

    Test2 Test3

---
+++ Test2.java
@@ -0,0 +1,9 @@
+public class Test2{
+     public static void main(String[ ] args){
+
+System.out.println("姓名:甘鑫");
+System.out.println("性别:男");
+System.out.println("籍贯:陕西");
+System.out.println("家庭住址:陕西省汉中市汉台区");
+}
+}
---
+++ Test3.java
@@ -0,0 +1,18 @@
+
+public class Test3{
+     public static void main(String[ ] args){
+
+System.out.println("   *                                                    *         ");
+System.out.println("*      *               I love You             *       *     ");
+System.out.println("   *                                                      *        ");
+System.out.println("       *                                              *            ");
+System.out.println("           *                                        *               ");
+System.out.println("               *                                *                   ");
+System.out.println("                   *                       *                        ");
+System.out.println("                       *               *                            ");
+System.out.println("                           *       *                                ");
+System.out.println("                                *                                   ");
+
+}
+}
+
