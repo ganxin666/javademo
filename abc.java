5da105249313b0121689058d60ce9cd52946134e
ganxin666 <1134896853@qq.com>
2020-06-20 15:17:25 +0800

    aaa

--- abc.java
+++ abc.java
@@ -1,10 +1,15 @@
 class Test {
           public static void main(String[ ] args){ 
   
-                System.out.println("hello 1!");
-                System.out.println("hello  22!");
-                System.out.println("hello   333!");
-                System.out.println("hello    4444!");
-                System.out.println("ÄãºÃ,ÊÀ½ç!");
+                System.out.println("*");
+                System.out.println("**");
+                System.out.println("***");
+                System.out.println("****");
+                System.out.println("*****");
+                System.out.println("******");
+                System.out.println("*******");
+                System.out.println("********");
+                System.out.println("*********");
+
                }
 }
