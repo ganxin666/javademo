d59df887cc2cff9a8c6049f712df27b3a6be2752
ganxin666 <1134896853@qq.com>
2020-06-20 15:47:57 +0800

    qwe

---
+++ HelloJava.java
@@ -0,0 +1,10 @@
+public class HelloJava{
+       /**
+             @auther Ays
+             @version 0.1
+*/
+   public static void main(string[ ] args) {
+  
+   System.out.println("args0:"args[0]);
+   System.out.println("args1:"args[1]);
+}
